63f66efd3980e8db7bfd696a12822460adb375ca
logan <logan@Squidputer.localdomain>
2025-09-26 10:26:12 -0400

    Realized carbon did not have argparse.

--- carbon.py
+++ carbon.py
@@ -2,6 +2,12 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+parser = argparse.ArgumentParser(description='Argument parser for carbon.py')
+
+parser.add_argument('--plot')
+
+args=parser.parse_args()
+
 tao=8223
 N0 = 10**(-9)*6.022*10**(23)
 
@@ -33,12 +39,19 @@ def realpdiff(stepsize,year):
     v2=N0*math.e**(-year/tao)
     return 2*abs(v1-v2)/(v1+v2)*100
 
-print(f'Relative difference between step size of 100 and 1000 is {pdiffatxyears(100, 1000, 5700*2):.2f}'+'%')
-print(f'Relative difference between step size of 10 and 100 is {pdiffatxyears(10, 100, 5700*2):.2f}'+'%')
-print(f'Asolute difference between real value and step size of 100 is {realpdiff(100,5700*2):.2f}'+'%')
-plotEQ(100,years)
-plotEQ(10,years)
-plotEQ(1000,years)
-plotEQ(5000,years)
+#print(f'Relative difference between step size of 100 and 1000 is {pdiffatxyears(100, 1000, 5700*2):.2f}'+'%')
+#print(f'Relative difference between step size of 10 and 100 is {pdiffatxyears(10, 100, 5700*2):.2f}'+'%')
+#print(f'Asolute difference between real value and step size of 100 is {realpdiff(100,5700*2):.2f}'+'%')
+#plotEQ(100,years)
+#plotEQ(10,years)
+#plotEQ(1000,years)
+#plotEQ(5000,years)
+
+if args.plot:
+    stepsize=args.plot
+    plotEQ(stepsize,years)
+else:
+    stepsize=100
+
 #plt.show()
-plt.savefig('carbon.png')
+plt.savefig(f'carbonWidth{stepsize}.png')
